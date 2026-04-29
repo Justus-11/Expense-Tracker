1d80099cfb218d394a545d7b7fab808e32be9ccd
Justus-11 <wambuakariuki04@gmail.com>
2026-04-30 01:27:20 +0300

    debug mongo env

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+
     if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is missing in Render environment variables");
+      throw new Error("MONGO_URI is missing in environment variables");
     }
 
     const conn = await mongoose.connect(process.env.MONGO_URI);
